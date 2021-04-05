4efd2a6772b342a2800d7f33bce08e8a4687aabf
Bill Eger <egeriicw@gmail.com>
2021-04-04 21:21:02 -0400

    Add state to App and pass to UserOutput component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import UserOutput from './Components/UserOutput/UserOutput';
 
 class App extends Component {
+  state = {
+    usernames: [
+      {username: "egeriicw"},
+      {username: "caroline"},
+      {username: "moose"},
+    ]
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,16 +19,17 @@ class App extends Component {
           <li>X UserInput should hold an input element, UserOutput two paragraphs</li>
           <li>X Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
           <li>X Pass a username (of your choice) to UserOutput via props and display it there</li>
-          <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
+          <li>X Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
           <li>Add a method to manipulate the state (=> an event-handler method)</li>
           <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
           <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
           <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
           <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
         </ul>
-        <UserOutput />
-        <UserOutput />
-        <UserOutput username="egeriicw" />
+        <hr />
+        <UserOutput username={this.state.usernames[0].username}/>
+        <UserOutput username={this.state.usernames[1].username}/>
+        <UserOutput username={this.state.usernames[2].username} />
       </div>
     );
   }
