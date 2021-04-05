f4705694ebaaef1e48b2d9af954aa3c229c81304
Bill Eger <egeriicw@gmail.com>
2021-04-04 20:59:09 -0400

    UserInput - input element, UserOutput 2 paragraphs

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <ul>
           <li>X Create TWO new components: UserInput and UserOutput</li>
-          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
+          <li>X UserInput should hold an input element, UserOutput two paragraphs</li>
           <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
           <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
           <li>Add state to the App component (=> the username) and pass the username to the UserOutput component</li>
