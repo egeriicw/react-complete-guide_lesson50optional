4f7516e8e10560e4114033a6b374955479010c84
Bill Eger <egeriicw@gmail.com>
2021-04-06 17:48:02 -0400

    Added styling

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ class App extends Component {
           <li>X Add a method to manipulate the state (=> an event-handler method)</li>
           <li>X Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
           <li>X Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
-          <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
-          <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
+          <li>X Add two-way-binding to your input (in UserInput) to also display the starting username</li>
+          <li>X Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
         </ul>
         <hr />
         <UserInput 
--- src/Components/UserInput/UserInput.js
+++ src/Components/UserInput/UserInput.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const userinput = (props) => {
+    
+    const style = {
+        border: "5px solid blue",
+    };
+    
     return (
-        <input type="text" 
+        <input type="text"
+            style={style} 
             onChange={props.changed} 
             value={props.currentName}
         />
